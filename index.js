3e81785d9a62b6db1fa4a43ff72b22ef04ef8995
Erik Luebbers <erluebbers@gmail.com>
2021-11-05 15:38:33 -0700

    commit tree b857c3b9b61eb744701dccd06fc7f34f47216472

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+// function superbowlWin (array) { 
+//     return array.find(function (array) {
+//         if (array.result === "W") {
+//             return array.year
+//         }
+//         })
+//   }
 
+  function superbowlWin (array) { 
+    let foundItem = array.find( function (obj) {
+        return obj.result === "W"
+    })
+    if (foundItem) {
+        return foundItem.year
+    } else {
+        return foundItem
+    }
+  }
